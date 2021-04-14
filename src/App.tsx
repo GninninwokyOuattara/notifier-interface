e5ea03ddd9d055359985ad94aad28b6aab1f8853
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-14 23:03:44 +0000

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import useBot from "./Hooks/useBot";
 
 function App() {
     console.log(process.env.NODE_ENV);
-    console.log(process.env.CHECK_STATE_URL);
-    console.log(process.env.REACT_APP_TEST);
+    console.log(process.env.REACT_APP_CHECK_STATE_URL);
+    console.log(process.env.REACT_APP_START_BOT_URL);
+
+    const [isRunning, setIsrunning] = useBot();
+    console.log(isRunning);
 
     return <h1>Hello World</h1>;
 }
