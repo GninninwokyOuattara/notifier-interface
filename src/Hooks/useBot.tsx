7677cf9f5986d7bfff947545c03f286e7761b2e6
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-15 01:29:20 +0000

    useBot hook

--- src/Hooks/useBot.tsx
+++ src/Hooks/useBot.tsx
@@ -1,18 +1,43 @@
 import React, { useState, useEffect } from "react";
+// import fetch from "cross-fetch";
 
-type Response = { running: boolean } | { error: string };
+type Response = { running: boolean | string };
 
 const checkStateUrl = process.env.REACT_APP_CHECK_STATE_URL as string;
+const startBot = process.env.REACT_APP_START_BOT_URL as string;
 
-const useBot = () => {
+const useBot = (): [
+    Response,
+    React.Dispatch<React.SetStateAction<Response>>
+] => {
     const [isRunning, setIsRunning] = useState<Response>({ running: false });
 
+    const runBot = () => {
+        fetch(startBot, {
+            method: "GET",
+            headers: {
+                "access-control-allow-origin": "*",
+                "Content-type": "application/json; charset=utf-8",
+            },
+        })
+            .then((res) => res.json())
+            .then((jsondata) => {
+                setIsRunning({ running: jsondata.running });
+            });
+    };
+
     useEffect(() => {
-        fetch(
-            `https://cors-anywhere.herokuapp.com/scantrad-wa-notifier-private.herokuapp.com/start`
-        ).then((res) => {
-            console.log(res);
-        });
+        fetch(checkStateUrl, {
+            method: "GET",
+            headers: {
+                "access-control-allow-origin": "*",
+                "Content-type": "application/json; charset=utf-8",
+            },
+        })
+            .then((res) => res.json())
+            .then((jsondata) => {
+                setIsRunning({ running: jsondata.running });
+            });
     }, []);
 
     return [isRunning, setIsRunning];
